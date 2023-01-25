0e7b9db69ee761a5d2d0a908dc7a3cb3e5f6f1da
John <tim@cruz-family.ws>
2023-01-25 08:41:34 -0800

    GET group info by id done

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { requireAuth } = require('../../utils/auth');
-const { Event, Membership, Group, GroupImage, User, Attendance, sequelize } = require('../../db/models');
+const { Event, Venue, Membership, Group, GroupImage, User, Attendance, sequelize } = require('../../db/models');
 const router = express.Router();
 
-router.get('/current', requireAuth, async(req, res, next) => {
+router.get('/current', requireAuth, async (req, res, next) => {
 
     const { user } = req;
 
@@ -40,7 +40,66 @@ router.get('/current', requireAuth, async(req, res, next) => {
     });
 })
 
-router.get('/', async(req, res, next) => {
+router.get('/:groupId', async (req, res, next) => {
+
+    const id = req.params.groupId;
+
+    const groupInfo = await Group.findByPk(id, {
+        include: [{
+            model: GroupImage,
+            attributes: {
+                exclude: [
+                    'createdAt',
+                    'updatedAt'
+                ]
+            }
+        }]
+    });
+
+    if (!groupInfo) {
+        const err = {};
+        err.message = 'Group couldn\'t be found';
+        err.statusCode = 404;
+        res.statusCode = 404;
+        res.json(err);
+    };
+
+
+    members = await Membership.findAll({
+        where: {
+            groupId: id
+        },
+    });
+
+    organizer = await User.findByPk(groupInfo.dataValues.organizerId, {
+        attributes: {
+            exclude: [
+                'username'
+            ]
+        }
+    });
+
+    vennueInfo = await Venue.findAll({
+        where: {
+            groupId: id
+        },
+        attributes: {
+            exclude: [
+                'createdAt',
+                'updatedAt'
+            ]
+        }
+    });
+
+    groupInfo.toJSON();
+    groupInfo.dataValues.Organizer = organizer;
+    groupInfo.dataValues.Venues = vennueInfo;
+    groupInfo.dataValues.numMembers = members.length;
+
+    res.json(groupInfo);
+})
+
+router.get('/', async (req, res, next) => {
     const allGroups = await Group.findAll();
 
     const groups = [];
