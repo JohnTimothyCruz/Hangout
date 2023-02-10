e96e372de96b20ef9b9943f4d3216e172a2751f0
John <tim@cruz-family.ws>
2023-02-10 10:34:48 -0800

    auth 4

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,44 +1,45 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import * as sessionActions from '../../store/session';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+function Navigation({ isLoaded }) {
+    const sessionUser = useSelector(state => state.session.user);
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
+    let sessionLinks;
+    if (sessionUser) {
+        sessionLinks = (
+            <li>
+                <ProfileButton user={sessionUser} />
+            </li>
+        );
+    } else {
+        sessionLinks = (
+            <li>
+                <OpenModalButton
+                    buttonText="Log In"
+                    modalComponent={<LoginFormModal />}
+                />
+                <OpenModalButton
+                    buttonText="Sign Up"
+                    modalComponent={<SignupFormModal />}
+                />
+            </li>
+        );
+    }
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <li>
-        <ProfileButton user={sessionUser} />
-        <button onClick={logout}>Log Out</button>
-      </li>
+    return (
+        <ul>
+            <li>
+                <NavLink exact to="/">Home</NavLink>
+            </li>
+            {isLoaded && sessionLinks}
+        </ul>
     );
-  } else {
-    sessionLinks = (
-      <li>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </li>
-    );
-  }
-
-  return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-      </li>
-      {isLoaded && sessionLinks}
-    </ul>
-  );
 }
 
 export default Navigation;
