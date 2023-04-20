ab0458096b7e6256973f97921417109aa986fa6b
John <tim@cruz-family.ws>
2023-04-20 16:01:38 -0700

    improved seed data

--- backend/db/seeders/20230124001936-demo-group.js
+++ backend/db/seeders/20230124001936-demo-group.js
@@ -22,49 +22,67 @@ module.exports = {
     await queryInterface.bulkInsert(options, [
       {
         organizerId: 1,
-        name: 'Group-One',
-        about: 'We\'re group one.',
+        name: 'Fullstack Projects',
+        about: 'We are a small group of fullstack developers who every month clone a website as a group! Whether you want to make connections in the industry, have more projects on your resume, or just want to flex your developer muscles, this is the group for you.',
         type: 'Online',
         private: true,
-        city: 'Onevale',
-        state: 'CA'
+        city: 'New Castle',
+        state: 'DE'
       },
       {
         organizerId: 2,
-        name: 'Group-Two',
-        about: 'We\'re group two.',
-        type: 'Online',
+        name: 'Outdoor Adventure Seekers',
+        about: 'Sometimes our lives can be a little much, and sometimes we need a little break from it. Together we will explore the outdoors and find some peace of mind. Whether you want to go on a short hike or go camping over the weekend, this group has an event for you.',
+        type: 'In Person',
         private: true,
-        city: 'Twovill',
-        state: 'AZ'
+        city: 'Mauston',
+        state: 'WI'
       },
       {
         organizerId: 3,
-        name: 'Group-Three',
-        about: 'We\'re group three.',
+        name: 'Pizza Lovers',
+        about: 'We are a group of people who love eating pizza. That\'s it. Plain and simple. Every week we meetup and find a new pizza place to try out.',
+        type: 'In Person',
+        private: true,
+        city: 'New York',
+        state: 'NY'
+      },
+      {
+        organizerId: 3,
+        name: 'Cooking Together!',
+        about: 'Do you want to hone your cooking skills? Wether you are an amateur or a professional, we find recipies to cook as a group.',
         type: 'In Person',
         private: false,
-        city: 'Threemouth',
-        state: 'AK'
+        city: 'Little Rock',
+        state: 'AR'
       },
       {
         organizerId: 4,
-        name: 'Group-Four',
-        about: 'We\'re group four.',
+        name: 'Communication Skills',
+        about: 'A lot of us have a hard time speaking to each other people. Especially after COVID, a lot of us haven\'t spoken to other people for a long time. This group was made for people like us to ease back into our social lives. We have an event every week where we have short seminars giving tips and tricks to improve our self-confidence and our ability to start conversations with others. We also have bi-weekly events where we play games and hang out. Come join us!',
+        type: 'Online',
+        private: false,
+        city: 'Anchorage',
+        state: 'AL'
+      },
+      {
+        organizerId: 4,
+        name: 'Book Reading Sessions',
+        about: 'Do you like books but have no money to buy them? Well on Mondays, Wednesdays, and Fridays at 6pm EST we have a reading of various books.',
         type: 'Online',
         private: true,
-        city: 'Fourshire',
-        state: 'MD'
+        city: 'Newark',
+        state: 'DE'
       },
       {
         organizerId: 5,
-        name: 'Group-Five',
-        about: 'We\'re group Five.',
+        name: 'Boardgame Group for Group Boardgames',
+        about: 'Do you have a boardgame that you want to play but have no interested friends? Well come on down! We come together four times a month to play those multiplayer boardgames we\'ve always wanted to play!',
         type: 'In Person',
         private: false,
-        city: 'Fiveburg',
-        state: 'PA'
-      }
+        city: 'Everett',
+        state: 'WA'
+      },
     ], {})
   },
 
--- backend/db/seeders/20230124195139-demo-eventImage.js
+++ backend/db/seeders/20230124195139-demo-eventImage.js
@@ -22,41 +22,80 @@ module.exports = {
     await queryInterface.bulkInsert(options, [
       {
         eventId: 1,
-        url: 'https://secure-content.meetupstatic.com/images/classic-events/510263871/222x125.webp',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/250px-YouTube_Logo_2017.svg.png',
         preview: true
       },
       {
         eventId: 2,
-        url: 'https://secure-content.meetupstatic.com/images/classic-events/510247037/222x125.webp',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Reddit_icon.svg/150px-Reddit_icon.svg.png',
         preview: true
       },
       {
         eventId: 3,
-        url: 'https://secure-content.meetupstatic.com/images/classic-events/509717215/222x125.webp',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/LinkedIn_2021.svg/200px-LinkedIn_2021.svg.png',
         preview: false
       },
       {
         eventId: 4,
-        url: 'https://secure-content.meetupstatic.com/images/classic-events/510456115/222x125.webp',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/MDN_Web_Docs_logo.svg/220px-MDN_Web_Docs_logo.svg.png',
         preview: true
       }, {
         eventId: 5,
-        url: 'https://secure-content.meetupstatic.com/images/classic-events/510511628/222x125.webp',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/220px-Spotify_logo_with_text.svg.png',
         preview: false
       },
       {
         eventId: 6,
-        url: 'https://secure-content.meetupstatic.com/images/classic-events/507304133/222x125.webp',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Kickstarter_logo_2019.svg/220px-Kickstarter_logo_2019.svg.png',
         preview: false
       },
       {
         eventId: 7,
-        url: 'https://secure-content.meetupstatic.com/images/classic-events/510479159/222x125.webp',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Hiking_to_the_Ice_Lakes._San_Juan_National_Forest%2C_Colorado.jpg/300px-Hiking_to_the_Ice_Lakes._San_Juan_National_Forest%2C_Colorado.jpg',
         preview: false
       },
       {
         eventId: 8,
-        url: 'https://secure.meetupstatic.com/next/images/fallbacks/group-cover-15-wide.webp',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Tent_camping_along_the_Sulayr_trail_in_La_Taha%2C_Sierra_Nevada_National_Park_%28DSCF5147%29.jpg/220px-Tent_camping_along_the_Sulayr_trail_in_La_Taha%2C_Sierra_Nevada_National_Park_%28DSCF5147%29.jpg',
+        preview: false
+      },
+      {
+        eventId: 9,
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/220px-Eq_it-na_pizza-margherita_sep2005_sml.jpg',
+        preview: true
+      },
+      {
+        eventId: 10,
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Black_Forest_gateau.jpg/250px-Black_Forest_gateau.jpg',
+        preview: true
+      },
+      {
+        eventId: 11,
+        url: 'https://nmcdn.io/e186d21f8c7946a19faed23c3da2f0da/556712d9bf0f4cb2a916cc810687d52b/files/blog-drafts/talking.png',
+        preview: false
+      },
+      {
+        eventId: 12,
+        url: 'https://nmcdn.io/e186d21f8c7946a19faed23c3da2f0da/556712d9bf0f4cb2a916cc810687d52b/files/blog-drafts/talking.png',
+        preview: false
+      },
+      {
+        eventId: 13,
+        url: 'https://nmcdn.io/e186d21f8c7946a19faed23c3da2f0da/556712d9bf0f4cb2a916cc810687d52b/files/blog-drafts/talking.png',
+        preview: true
+      }, {
+        eventId: 14,
+        url: 'https://nmcdn.io/e186d21f8c7946a19faed23c3da2f0da/556712d9bf0f4cb2a916cc810687d52b/files/blog-drafts/talking.png',
+        preview: false
+      },
+      {
+        eventId: 15,
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvbHgztZzrbHP-lwbZrRXIwChwUJh9g9QNxA&usqp=CAU',
+        preview: false
+      },
+      {
+        eventId: 16,
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbWMwFgcYFDCq46r9UIxm1sfq3a1P3ifVbxQ&usqp=CAU',
         preview: false
       },
     ], {})
