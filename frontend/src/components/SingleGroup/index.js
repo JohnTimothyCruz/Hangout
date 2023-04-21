b8a433172869c6f7e3a440637926f3acf0c3c768
John <tim@cruz-family.ws>
2023-04-21 09:16:10 -0700

    added single group loading styles

--- frontend/src/components/SingleGroup/index.js
+++ frontend/src/components/SingleGroup/index.js
@@ -72,113 +72,155 @@ const SingleGroup = () => {
         dispatch(clearEvent())
     }, [])
 
-    if (group === undefined || group === null || !Object.values(group).length) return null;
+    let upcomingEvents;
+    let pastEvents;
+    let anyUpcoming;
+    let anyPast;
 
-    const upcomingEvents = allUpcomingEvents(group);
-    const pastEvents = allPastEvents(group);
+    if (group !== undefined && group !== null && Object.values(group).length) {
+        upcomingEvents = allUpcomingEvents(group);
+        pastEvents = allPastEvents(group);
 
-    let anyUpcoming = false;
-    Object.values(upcomingEvents).length ? anyUpcoming = true : anyUpcoming = false;
+        anyUpcoming = false;
+        Object.values(upcomingEvents).length ? anyUpcoming = true : anyUpcoming = false;
 
-    let anyPast = false;
-    Object.values(pastEvents).length ? anyPast = true : anyPast = false;
+        anyPast = false;
+        Object.values(pastEvents).length ? anyPast = true : anyPast = false;
+    }
 
     return (
-        <div className='group-body'>
-            <div className='group-main-top'>
-                <div className='group-groups-redirect-container'>
-                    <h4 className='group-arrow'>{'<'}</h4>
-                    <NavLink to='/groups' className='group-groups-redirect'>Groups</NavLink>
-                </div>
-                <div className='group-main-middle'>
-                    <img src={group.GroupImages['0'].url} alt='group' className='group-middle-left SingleGroup-group-image'></img>
-                    <div className='group-middle-right'>
-                        <div className='group-middle-right-top'>
-                            <h2 className='group-name'>{group.name}</h2>
-                            <div className='group-group-details'>
-                                <h4>{group.city}, {group.state}</h4>
-                                <h4>{Object.values(group.Events).length} events · {group.private ? 'Private' : 'Public'}</h4>
-                                <h4>Organized by {group.Organizer.firstName} {group.Organizer.lastName}</h4>
+        Object.values(group).length ?
+            <div className='group-body'>
+                <div className='group-main-top'>
+                    <div className='group-groups-redirect-container'>
+                        <h4 className='group-arrow'>{'<'}</h4>
+                        <NavLink to='/groups' className='group-groups-redirect'>Groups</NavLink>
+                    </div>
+                    <div className='group-main-middle'>
+                        <img src={group.GroupImages['0'].url} alt='group' className='group-middle-left SingleGroup-group-image'></img>
+                        <div className='group-middle-right'>
+                            <div className='group-middle-right-top'>
+                                <h2 className='group-name'>{group.name}</h2>
+                                <div className='group-group-details'>
+                                    <h4>{group.city}, {group.state}</h4>
+                                    <h4>{Object.values(group.Events).length} events · {group.private ? 'Private' : 'Public'}</h4>
+                                    <h4>Organized by {group.Organizer.firstName} {group.Organizer.lastName}</h4>
+                                </div>
                             </div>
-                        </div>
-                        <div className='group-middle-right-bottom'>
-                            {
-                                (user && user.id === group.Organizer.id) ?
-                                    <div className='group-button-container'>
-                                        <NavLink to={`/groups/${id}/events/new`} className='group-create-event-button'>Create event</NavLink>
-                                        <NavLink to={`/groups/${id}/edit`} className='group-update-event-button'>Update</NavLink>
-                                        <OpenModalMenuItem
-                                            className='group-delete-event-button'
-                                            itemText="Delete"
-                                            modalComponent={<DeleteGroupModal />}
-                                        />
-                                    </div> :
-                                    <div className='group-join-button' onClick={onClick}>Join this group</div>
-                            }
+                            <div className='group-middle-right-bottom'>
+                                {
+                                    (user && user.id === group.Organizer.id) ?
+                                        <div className='group-button-container'>
+                                            <NavLink to={`/groups/${id}/events/new`} className='group-create-event-button'>Create event</NavLink>
+                                            <NavLink to={`/groups/${id}/edit`} className='group-update-event-button'>Update</NavLink>
+                                            <OpenModalMenuItem
+                                                className='group-delete-event-button'
+                                                itemText="Delete"
+                                                modalComponent={<DeleteGroupModal />}
+                                            />
+                                        </div> :
+                                        <div className='group-join-button' onClick={onClick}>Join this group</div>
+                                }
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='group-main-bottom'>
-                <div className='group-organizer'>
-                    <h2>Organizer</h2>
-                    <h5>{group.Organizer.firstName} {group.Organizer.lastName}</h5>
-                </div>
-                <div className='group-about'>
-                    <h2>What we're about</h2>
-                    <p>{group.about}</p>
-                </div>
-                <div className='group-no-events'>
-                    {
-                        (!anyPast && !anyUpcoming) && <h2>No Upcoming Events</h2>
-                    }
-                </div>
-                <div className={`group-upcoming-events ${anyUpcoming ? '' : 'hidden'}`}>
-                    <h2 className={anyUpcoming ? '' : 'hidden'}>Upcoming Events ({upcomingEvents.length})</h2>
-                    {
-                        anyUpcoming && upcomingEvents.map((event, idx) => {
-                            return (
-                                <div className='group-event-card' key={idx}>
-                                    <NavLink to={`/events/${event.id}`} className='group-event-link'>
-                                        <div className='group-event-card-top'>
-                                            <img src={event.url} alt='event' className='group-card-top-left SingleGroup-event-image'></img>
-                                            <div className='group-card-top-left'>
-                                                <h4 className='group-card-time'>{getStartTime(event)}</h4>
-                                                <h3 className='group-card-title'>{event.name}</h3>
-                                                <h4 className='group-card-location'>{event.Venue.city}, {event.Venue.state}</h4>
+                <div className='group-main-bottom'>
+                    <div className='group-organizer'>
+                        <h2>Organizer</h2>
+                        <h5>{group.Organizer.firstName} {group.Organizer.lastName}</h5>
+                    </div>
+                    <div className='group-about'>
+                        <h2>What we're about</h2>
+                        <p>{group.about}</p>
+                    </div>
+                    <div className='group-no-events'>
+                        {
+                            (!anyPast && !anyUpcoming) && <h2>No Upcoming Events</h2>
+                        }
+                    </div>
+                    <div className={`group-upcoming-events ${anyUpcoming ? '' : 'hidden'}`}>
+                        <h2 className={anyUpcoming ? '' : 'hidden'}>Upcoming Events ({upcomingEvents.length})</h2>
+                        {
+                            anyUpcoming && upcomingEvents.map((event, idx) => {
+                                return (
+                                    <div className='group-event-card' key={idx}>
+                                        <NavLink to={`/events/${event.id}`} className='group-event-link'>
+                                            <div className='group-event-card-top'>
+                                                <img src={event.url} alt='event' className='group-card-top-left SingleGroup-event-image'></img>
+                                                <div className='group-card-top-left'>
+                                                    <h4 className='group-card-time'>{getStartTime(event)}</h4>
+                                                    <h3 className='group-card-title'>{event.name}</h3>
+                                                    <h4 className='group-card-location'>{event.Venue.city}, {event.Venue.state}</h4>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className='group-event-card-description'>{event.description}</div>
-                                    </NavLink>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-                <div className={`group-past-events ${anyUpcoming ? '' : 'hidden'}`}>
-                    <h2 className={anyPast ? '' : 'hidden'}>Past Events ({pastEvents.length})</h2>
-                    {
-                        anyPast && pastEvents.map((event, idx) => {
-                            return (
-                                <div className='group-event-card' key={idx}>
-                                    <NavLink to={`/events/${event.id}`} className='group-event-link'>
-                                        <div className='group-event-card-top'>
-                                            <img src={event.url} alt='event' className='group-card-top-left SingleGroup-event-image'></img>
-                                            <div className='group-card-top-left'>
-                                                <h4 className='group-card-time'>{getStartTime(event)}</h4>
-                                                <h3 className='group-card-title'>{event.name}</h3>
-                                                <h4 className='group-card-location'>{event.Venue.city}, {event.Venue.state}</h4>
+                                            <div className='group-event-card-description'>{event.description}</div>
+                                        </NavLink>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className={`group-past-events ${anyUpcoming ? '' : 'hidden'}`}>
+                        <h2 className={anyPast ? '' : 'hidden'}>Past Events ({pastEvents.length})</h2>
+                        {
+                            anyPast && pastEvents.map((event, idx) => {
+                                return (
+                                    <div className='group-event-card' key={idx}>
+                                        <NavLink to={`/events/${event.id}`} className='group-event-link'>
+                                            <div className='group-event-card-top'>
+                                                <img src={event.url} alt='event' className='group-card-top-left SingleGroup-event-image'></img>
+                                                <div className='group-card-top-left'>
+                                                    <h4 className='group-card-time'>{getStartTime(event)}</h4>
+                                                    <h3 className='group-card-title'>{event.name}</h3>
+                                                    <h4 className='group-card-location'>{event.Venue.city}, {event.Venue.state}</h4>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className='group-event-card-description'>{event.description}</div>
-                                    </NavLink>
-                                </div>
-                            )
-                        })
-                    }
+                                            <div className='group-event-card-description'>{event.description}</div>
+                                        </NavLink>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
+            :
+            <div className='group-body'>
+                <div className='group-main-top'>
+                    <div className='group-groups-redirect-container'>
+                        <h4 className='group-arrow'>{'<'}</h4>
+                        <div to='/groups' className='group-groups-redirect'>Groups</div>
+                    </div>
+                    <div className='group-main-middle'>
+                        <div className='empty-group-image empty-and-loading'></div>
+                        <div className='group-middle-right'>
+                            <div className='group-middle-right-top'>
+                                <h2 className='empty-group-name empty-and-loading'></h2>
+                                <div className='group-group-details'>
+                                    <h4 className='empty-group-location empty-and-loading'></h4>
+                                    <h4 className='empty-group-events-count empty-and-loading'></h4>
+                                    <h4 className='empty-group-organizer empty-and-loading'></h4>
+                                </div>
+                            </div>
+                            <div className='group-middle-right-bottom'></div>
+                        </div>
+                    </div>
+                </div>
+                <div className='group-main-bottom'>
+                    <div className='group-organizer'>
+                        <h2>Organizer</h2>
+                        <h5 className='empty-group-organizer-small empty-and-loading'></h5>
+                    </div>
+                    <div className='group-about'>
+                        <h2>What we're about</h2>
+                        <p className='empty-group-about empty-and-loading'></p>
+                    </div>
+                    <div className='group-no-events'>
+                        <h2>No Upcoming Events</h2>
+                    </div>
+                </div>
+            </div>
     )
 }
 export default SingleGroup
