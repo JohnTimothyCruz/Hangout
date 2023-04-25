728efc0cdc490f1acab1563c5bd2501018c4ed8e
John <tim@cruz-family.ws>
2023-04-25 15:10:56 -0700

    group images are ddeleteable

---
+++ frontend/src/components/SingleGroupImage/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteGroupImage } from '../../store/groupReducer';
+import './SingleGroupImage.css'
+
+const SingleGroupImage = ({ image }) => {
+    const dispatch = useDispatch();
+    const ulRef = useRef();
+    const [showMenu, setShowMenu] = useState(false);
+
+    useEffect(() => {
+        console.log(showMenu)
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (!ulRef.current?.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener("click", closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const handleGroupImageDelete = async (id) => {
+        dispatch(deleteGroupImage(id))
+    }
+
+    return (
+        <div className='group-images-image-container'>
+            <i className="fa-solid fa-ellipsis group-images-image-ellipsis" onClick={() => setShowMenu(!showMenu)} />
+            <img src={image.url} className='group-images-image' />
+            <p>{image?.description}</p>
+            <div className={showMenu ? "group-images-image-delete" : "hidden"} onClick={() => handleGroupImageDelete(image?.id)}>Delete Image</div>
+        </div>
+    )
+}
+
+export default SingleGroupImage
